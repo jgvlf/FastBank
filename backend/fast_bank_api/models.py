52eb821606d1d985e6b44144e4b01b578f62ae01
joaogabrielvlf <joaogabrielvlf@gmail.com>
2022-09-19 09:54:07 -0300

    👌 IMPROVE:  Create a User Model

--- backend/fast_bank_api/models.py
+++ backend/fast_bank_api/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Usuario(models.Model):
+    cpf = models.CharField(max_length = 11, unique=True)
+    password = models.CharField()
+    
+    def __str__(self):
+        return self.cpf
+    
+    
+    class Meta:
+        verbose_name_plural = "Usuarios"
+
+            
