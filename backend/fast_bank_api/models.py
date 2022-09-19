651ee7a60b2bc0946fbe2b92d8e259d8fbd40c06
joaogabrielvlf <joaogabrielvlf@gmail.com>
2022-09-19 09:54:32 -0300

    👌 IMPROVE: Create a Addres Model

--- backend/fast_bank_api/models.py
+++ backend/fast_bank_api/models.py
@@ -13,3 +13,18 @@ class Usuario(models.Model):
         verbose_name_plural = "Usuarios"
 
             
+class Endereco(models.Model):
+    country = models.CharField(max_length = 255)
+    uf = models.CharField(max_length = 2)
+    city = models.CharField(max_length = 255)
+    neighborhood = models.CharField(max_length= 255)
+    road = models.CharField(max_length = 255)
+    house_number = models.CharField(max_length=10)
+    cep = models.CharField(max_length = 8)
+    
+    def __str__(self):
+        return self.id
+    
+    
+    class Meta:
+        verbose_name_plural = "Enderecos"
