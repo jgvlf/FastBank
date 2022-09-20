a289a3953073c1cba08858ff08e994545a10edb8
joaogabrielvlf <joaogabrielvlf@gmail.com>
2022-09-20 11:28:58 -0300

    👌 IMPROVE: Set Mobile Width Devices

--- frontend/core-front/tailwind.config.cjs
+++ frontend/core-front/tailwind.config.cjs
@@ -5,6 +5,27 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    screens: {
+      
+      'mobileS': '280px',
+      // => @media (min-width: 280px) { ... }
+
+      'mobileM': '375px',
+      // => @media (min-width: 393px) { ... }
+
+      'mobileL': '412px',
+      // => @media (min-width: 412px) { ... }
+      
+      'tablet': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'tablet': '640px',
+      // => @media (min-width: 640px) { ... }
+      
+      'default': '1280px',
+      // => @media (min-width: 1920px) { ... }
+
+    },
     extend: {},
   },
   plugins: [],
