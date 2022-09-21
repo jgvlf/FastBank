f6102fdd1732acbe892298657f27f3a086aaf2b9
joaogabrielvlf <joaogabrielvlf@gmail.com>
2022-09-21 07:52:08 -0300

    👌 IMPROVE: Better Organization

--- frontend/core-front/src/App.jsx
+++ frontend/core-front/src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import Home from './base/home/Home';
+import Home from './pages/Home';
 
 function App() {
 
--- frontend/core-front/src/pages/Home.jsx
+++ frontend/core-front/src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import React, {Component} from "react";
-import Header from './header/Header';
-import Footer from './footer/Footer';
-import Body from './body/Body';
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+import Body from '../components/Body';
 
 class Home extends Component {
     state = {  } 
