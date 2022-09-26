4d8e403503f87ec411b7e85960c85db768a51c5c
joaogabrielvlf <joaogabrielvlf@gmail.com>
2022-09-26 10:41:38 -0300

    👌 IMPROVE: Create Client View

--- backend/fast_bank_api/views.py
+++ backend/fast_bank_api/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import *
+from .serializer import *
 
 # Create your views here.
+
+class ClientesViewSets(viewsets.ModelViewSet):
+    queryset = Cliente.objects.all()
+    serializer_class = ClienteSerializer
