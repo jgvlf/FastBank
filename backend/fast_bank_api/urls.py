0341d354e902cd57d2f0ff285cf9430be7d1297f
joaogabrielvlf <joaogabrielvlf@gmail.com>
2022-09-26 10:41:22 -0300

    👌 IMPROVE: Add URL's to acess admin

--- backend/fast_bank_api/urls.py
+++ backend/fast_bank_api/urls.py
@@ -3,5 +3,5 @@ from rest_framework_nested.routers import DefaultRouter
 from django.urls import path, include
 
 rotas = DefaultRouter()
-rotas.register('clientes', )
+rotas.register('clientes', views.ClientesViewSets, basename='clientes')
 urlpatterns = rotas.urls
