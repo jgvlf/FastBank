98e7cafbc462ee74127e58cf9c21aa4dfe95fe71
joaogabrielvlf <joaogabrielvlf@gmail.com>
2022-09-26 10:40:59 -0300

    👌 IMPROVE: Add to Admin Site

--- backend/fast_bank_api/admin.py
+++ backend/fast_bank_api/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+
+admin.site.register(Usuario)
+admin.site.register(Endereco)
+admin.site.register(Conta)
+admin.site.register(Transferencia)
+admin.site.register(Emprestimo)
+admin.site.register(PGTO_Emprestimo)
+admin.site.register(SemBeneficio)
+admin.site.register(PlanoSaude)
+admin.site.register(ValeRefeicao)
+admin.site.register(ValeAlimentacao)
+admin.site.register(Cliente)
+admin.site.register(Beneficio)
+admin.site.register(Cartao)
+admin.site.register(Fatura)
