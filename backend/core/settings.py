51fa274e706cf2d31775035522af0a6954d7b3be
joaogabrielvlf <joaogabrielvlf@gmail.com>
2022-10-10 10:28:10 -0300

    👌 IMPROVE: Add a functional fetch api

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -38,11 +38,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'rest_framework',
     'fast_bank_api',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -53,6 +56,8 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'core.urls'
+
+CORS_ALLOW_ALL_ORIGINS = True
 
 TEMPLATES = [
     {
--- frontend/core-front/src/components/Body.jsx
+++ frontend/core-front/src/components/Body.jsx
@@ -1,13 +1,40 @@
 import React, {Component} from "react";
 
+
 class Body extends Component {
-    state = {  } 
-    render() { 
+    constructor(){
+        super();
+        this.state={
+            data:[]
+        };
+    }
+
+        async fetchData(){
+            await fetch('http://127.0.0.1:8000/api/clientes/')
+            .then(response=>response.json())
+            .then((data)=>{
+                this.setState({
+                    data:data
+                });
+            });
+        }
+
+    componentDidMount(){
+        this.fetchData();
+    }
+     
+    render() {
+        const empData = this.state.data;
+        const rows = empData.map((emp)=>
+            <div key={1}>
+                <h1>{emp.last_name}</h1>
+                <h1>{emp.first_name}</h1>
+            </div>
+        );  
         return (
             <>
-                <div className=" w-screen h-full bg-[#023E7D] mobileS:h-screen default:w-screen default:h-screen">
-                    
-                
+                <div className=" mt-[70px] w-screen h-full bg-[#023E7D] mobileS:h-screen default:w-screen default:h-screen">
+                    {rows}
                 </div>
 
             </>
