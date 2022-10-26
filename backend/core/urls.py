69c970d548d04c8bc4bd32b576254b90a377cad6
joaogabrielvlf <joaogabrielvlf@gmail.com>
2022-10-26 10:19:21 -0300

    👌 IMPROVE: Add the "Simple JWT" Page

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -20,4 +20,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('fast_bank_api.urls')),
     path('auth/', include('djoser.urls')),
+    path('auth/', include('djoser.urls.jwt')),
 ]
