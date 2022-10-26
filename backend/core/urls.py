9f73257ed8ab1ac752cfa4d4633b52fd183f5563
joaogabrielvlf <joaogabrielvlf@gmail.com>
2022-10-26 08:26:18 -0300

    👌 IMPROVE: Add the url

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('fast_bank_api.urls')),
+    path('auth/', include('djoser.urls')),
 ]
