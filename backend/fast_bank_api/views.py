4758b1c5e3746df23e0b4b9543784b197caf5461
joaogabrielvlf <joaogabrielvlf@gmail.com>
2022-11-16 08:25:46 -0300

    👌 IMPROVE: Add new Views

--- backend/fast_bank_api/views.py
+++ backend/fast_bank_api/views.py
@@ -23,5 +23,45 @@ class EnderecosViewSets(viewsets.ModelViewSet):
 class ContasViewSets(viewsets.ModelViewSet):
     queryset = Conta.objects.all()
     serializer_class = ContaSerializer
+
+class TransferenciaViewSets(viewsets.ModelViewSet):
+    queryset = Transferencia.objects.all()
+    serializer_class = TransferenciaSerializer
+
+class EmprestimoViewSets(viewsets.ModelViewSet):
+    queryset = Emprestimo.objects.all()
+    serializer_class = EmprestimoSerializer
+
+class PGTO_EmprestimoViewSets(viewsets.ModelViewSet):
+    queryset = PGTO_Emprestimo.objects.all()
+    serializer_class = PGTO_EmprestimoSerializer
+
+class SemBeneficioViewSets(viewsets.ModelViewSet):
+    queryset = SemBeneficio.objects.all()
+    serializer_class = SemBeneficioSerializer
+
+class PlanoSaudeViewSets(viewsets.ModelViewSet):
+    queryset = PlanoSaude.objects.all()
+    serializer_class = PlanoSaudeSerializer
+
+class ValeRefeicaoViewSets(viewsets.ModelViewSet):
+    queryset = ValeRefeicao.objects.all()
+    serializer_class = ValeRefeicaoSerializer
+
+class ValeAlimentacaoViewSets(viewsets.ModelViewSet):
+    queryset = ValeAlimentacao.objects.all()
+    serializer_class = ValeAlimentacaoSerializer
+
+class BeneficioViewSets(viewsets.ModelViewSet):
+    queryset = Beneficio.objects.all()
+    serializer_class = BeneficioSerializer
+
+class CartaoViewSets(viewsets.ModelViewSet):
+    queryset = Cartao.objects.all()
+    serializer_class = CartaoSerializer
+
+class FaturaViewSets(viewsets.ModelViewSet):
+    queryset = Fatura.objects.all()
+    serializer_class = FaturaSerializer
     
     
