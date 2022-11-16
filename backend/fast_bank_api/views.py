3a8395b209c36ab6c3597a2913d5d27488be54af
joaogabrielvlf <joaogabrielvlf@gmail.com>
2022-11-16 09:26:56 -0300

    📦 NEW:  🌟 Feat. Money Transitions Working.

--- backend/fast_bank_api/views.py
+++ backend/fast_bank_api/views.py
@@ -1,3 +1,4 @@
+import decimal
 from django.shortcuts import render
 from rest_framework import viewsets
 from .models import *
@@ -27,6 +28,39 @@ class ContasViewSets(viewsets.ModelViewSet):
 class TransferenciaViewSets(viewsets.ModelViewSet):
     queryset = Transferencia.objects.all()
     serializer_class = TransferenciaSerializer
+    def create(self, request, *args, **kwargs):
+        conta_1 = Conta.objects.get(id=self.request.data["sending_account"])
+        conta_2 = Conta.objects.get(id=self.request.data["recive_account"])
+        print(conta_1)
+        print(conta_2)
+        conta_1.money -= decimal.Decimal(self.request.data["value"])
+        conta_2.money += decimal.Decimal(self.request.data["value"])
+        conta_1_atualizar = {"agency": conta_1.agency, 
+                                "account_number": conta_1.account_number, 
+                                "verify_digit": conta_1.verify_digit,
+                                "money":conta_1.money,
+                                "class_account": conta_1.class_account,
+                                "account_type": conta_1.account_type,
+                                }
+        conta_2_atualizar = {"agency": conta_2.agency, 
+                                "account_number": conta_2.account_number, 
+                                "verify_digit": conta_2.verify_digit,
+                                "money":conta_2.money,
+                                "class_account": conta_2.class_account,
+                                "account_type": conta_2.account_type,
+                                }
+        serializer_1 = ContaSerializer(conta_1, data=conta_1_atualizar)
+        if serializer_1.is_valid():
+            serializer_1.save()
+        else:
+            print(serializer_1.errors)
+        serializer_2 = ContaSerializer(conta_2, data=conta_2_atualizar)
+        if serializer_2.is_valid():
+            serializer_2.save()
+        else:
+            print(serializer_2.errors)           
+            
+        return super().create(request, *args, **kwargs)
 
 class EmprestimoViewSets(viewsets.ModelViewSet):
     queryset = Emprestimo.objects.all()
